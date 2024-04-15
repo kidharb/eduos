5f90e5964d6ae1777d094c615bbb5dd9ed133b1a
Kidhar Bachan <kidharb@gmail.com>
2024-04-15 21:54:06 +0200

    Add cmake toolchain config

---
+++ cmake/toolchain.cmake
@@ -0,0 +1,15 @@
+SET(CMAKE_SYSTEM_NAME Generic-ELF)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   x86_64-elf-gcc)
+
+# where is the target environment
+SET(CMAKE_FIND_ROOT_PATH  /home/kidharb/opt/cross)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
