20bcb2bf47ada32c23e30038a9c997944a85d2f8
Kidhar Bachan <kidharb@gmail.com>
2024-04-20 21:44:38 +0200

    Add hdd image generation

--- cmake/limine.cmake
+++ cmake/limine.cmake
@@ -1,3 +1,30 @@
+find_program(qemu     NAMES qemu-system-x86_64 DOC "QEMU for x86_64 emulation")
+set(QEMU_MEMORY 512M)
+set(QEMU_UEFI       -net none -drive if=pflash,format=raw,readonly=on,file=${ovmf_firmware} -m ${QEMU_MEMORY} -machine q35)
+set(QEMU_FLAGS      -drive format=qcow2,file=firmware.qcow2,if=pflash,readonly=on -m ${QEMU_MEMORY} -machine q35 -d int,guest_errors --serial file:log.txt -device qemu-xhci --device isa-debugcon,iobase=0x402,chardev=debug -chardev file,id=debug,path=debug.log)
+
+add_custom_target(${IMAGE_NAME}
+	COMMAND rm -f ${IMAGE_NAME}.hdd
+	COMMAND dd if=/dev/zero bs=1M count=0 seek=64 of=${IMAGE_NAME}.hdd
+	COMMAND sgdisk ${IMAGE_NAME}.hdd -n 1:2048 -t 1:ef00
+    COMMAND make -C _deps/limine-src/
+	COMMAND ./_deps/limine-src/limine bios-install ${IMAGE_NAME}.hdd
+	COMMAND mformat -i ${IMAGE_NAME}.hdd@@1M
+	COMMAND mmd -i ${IMAGE_NAME}.hdd@@1M ::/EFI ::/EFI/BOOT ::/boot ::/boot/limine
+	COMMAND mcopy -i ${IMAGE_NAME}.hdd@@1M src/kernel/kernel.elf ::/boot
+	COMMAND mcopy -i ${IMAGE_NAME}.hdd@@1M ../limine.cfg _deps/limine-src/limine-bios.sys ::/boot/limine
+	COMMAND mcopy -i ${IMAGE_NAME}.hdd@@1M _deps/limine-src/BOOTX64.EFI ::/EFI/BOOT
+	COMMAND mcopy -i ${IMAGE_NAME}.hdd@@1M _deps/limine-src/BOOTIA32.EFI ::/EFI/BOOT
+    DEPENDS kernel
+    USES_TERMINAL
+)
+
+add_custom_target(run
+	COMMAND ${qemu} -M q35 -m 2G -hda ${IMAGE_NAME}.hdd
+    DEPENDS ${IMAGE_NAME}
+    USES_TERMINAL
+)
+
 #add_custom_command(
 #    OUTPUT firmware.qcow2
 #    COMMAND ${qemu_img} create -o backing_file=${ovmf_firmware} -o backing_fmt=raw -o cluster_size=512 -f qcow2 firmware.qcow2
